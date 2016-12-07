8b8d713cc00f9e1ce4121aa6976e69d3ec6ef5d2
chandra <chandra@192.168.0.11>
2016-12-07 09:50:40 -0500

    Mail Commit

--- src/main/java/algorithms/mail/MailConstants.java
+++ src/main/java/algorithms/mail/MailConstants.java
@@ -5,21 +5,33 @@ import java.util.Properties;
 public class MailConstants {
 
 	Properties props = new Properties();
-	protected final String recruiter_email_file_location = "C:\\Users\\chand\\Desktop\\Recruiters_emails_2.txt";
+	protected final String recruiter_email_file_location = "/Users/chandra/Documents/Recruiter_emails_2.txt";
 	protected final String username = "chandrahasjava21";
 	protected final String password = "kurosakinaruto";
 	protected final String from_email = "chandrahasjava21@gmail.com";
-	protected final String subject = "Looking for New Grad Software Positions: 2+ years of experience at startups";
+	protected final String attachment = "/Users/chandra/Documents/Resume_v2.2.pdf";
+	protected final String subject = "Looking for Full Time Software Positions: 2+ years of experience at startups";
 	protected final String body = "Hi,"
-			+ "\nI'm doing my Masters in Computer Science at University of South Carolina, Columbia. I'm looking for full time software developer opportunities."
-			+ "\nI recently worked as software developer at university of south carolina and previously worked as software engineer at Peel India(startup)."
-			+ "\nI have a background that suits positions related to  " + "\n1)Full stack development  "
-			+ "\n2)Big Data application development - using Apache Storm, Kafka, Amazon Kinesis  "
-			+ "\n3)Backend developer - Java & Python   "
-			+ "\n\nCan you kindly review my profile and let me know if you find me suitable for any positions."
-			+ "\nPlease find the attached resume."
-			+ "\n https://drive.google.com/file/d/0B_BW4gLgGMS-ZkZCM2k2ZVk0RVU/view   "
-			+ "\n\nP.S. If your team doesn't have any suitable positions for me, can you kindly forward my resume to other teams who might find me a good fit?";
+			+ "\nI am a graduate in Computer Science at University of South Carolina, Columbia. I'm looking for full time software developer/testing/operations opportunities. "
++ "I recently worked as software developer at university of south carolina and previously worked as software engineer at Peel Technologies."
++ "\n\nI have a background that suits positions related to"
++ "\n1)Full stack development"
++ "\n2)Big Data application development - Apache Storm, AWS services, Apache Kafka"
++ "\n3)Backend - Java & Python"
++ "\n4)Web Development - Rest services using Jersey, Play, AngularJS, Flask"
++ "\n5)Database - MySql, PostgreSql, MongoDB, Redshift, Cassandra"
++ "\n\nCan you kindly review my profile and let me know if you find me suitable for any positions."
++ "\nPlease find the attached resume."
+			+ "\n https://drive.google.com/file/d/0B_BW4gLgGMS-RjR4cTBuM0JoQ1U/view   "
+			+ "\n\nP.S. If your team doesn't have any suitable positions for me, can you kindly forward my resume to other teams who might find me a good fit. "
+			+ "\n\n\n-- "
+			+ "\nRegards,"
+			+ "\nChandrahas Raj G Venkat"
+			+ "\nMS Computer Science - Class of 2016​"
+			+ "\nUniversity of South Carolina, Columbia"
+			+ "\nm | (803)-445-4191"
+			+ "\nSkype | chandrahasraj@outlook.com"
+			+ "\nLinkedin | https://www.linkedin.com/in/chandrahas-raj-40a70321";
 
 	MailConstants() {
 		props.put("mail.smtp.host", "smtp.gmail.com");
@@ -27,5 +39,6 @@ public class MailConstants {
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.starttls.enable", "true");
 	}
 }
--- src/main/java/algorithms/mail/MailController.java
+++ src/main/java/algorithms/mail/MailController.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.apache.commons.io.FileUtils;
 
@@ -18,6 +21,7 @@ public class MailController extends MailConstants {
 	public static void main(String as[]) throws IOException {
 
 		MailController controller = new MailController();
+	System.out.println(controller.recruiter_email_file_location);
 		List<String> recruiters_emails = FileUtils.readLines(new File(controller.recruiter_email_file_location));
 		for (String email : recruiters_emails) {
 			Session session = controller.getSession();
@@ -26,7 +30,35 @@ public class MailController extends MailConstants {
 				message.setFrom(new InternetAddress(controller.from_email));
 				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
 				message.setSubject(controller.subject);
-				message.setText(controller.body);
+//				message.setText(controller.body);
+				
+				// creates message part
+		        MimeBodyPart messageBodyPart = new MimeBodyPart();
+		        messageBodyPart.setText(controller.body);
+		        String[] attachFiles = new String[]{controller.attachment};
+		 
+		        // creates multi-part
+		        Multipart multipart = new MimeMultipart();
+		        multipart.addBodyPart(messageBodyPart);
+		 
+		        // adds attachments
+		        if (attachFiles != null && attachFiles.length > 0) {
+		            for (String filePath : attachFiles) {
+		                MimeBodyPart attachPart = new MimeBodyPart();
+		 
+		                try {
+		                    attachPart.attachFile(filePath);
+		                } catch (IOException ex) {
+		                    ex.printStackTrace();
+		                }
+		 
+		                multipart.addBodyPart(attachPart);
+		            }
+		        }
+		 
+		        // sets the multi-part as e-mail's content
+		        message.setContent(multipart);
+		 
 
 				Transport.send(message);
 
